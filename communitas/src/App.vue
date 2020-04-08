518143a07e4ee4d18e0c2dbe53707fca30aebea4
jp aulet <blakdrak@gmail.com>
2020-04-08 14:11:42 +0200

    Added build config, deleted console.logs

--- communitas/src/App.vue
+++ communitas/src/App.vue
@@ -8,29 +8,24 @@ export default {
   created() {
     let pushed = false;
     const currentPath = this.$router.history.current.path;
-    alert(currentPath);
 
     if (window.localStorage.getItem('authenticated') === 'false') {
-      alert('dddd');
       pushed = true;
       this.$router.push('/');
     }
 
     if (currentPath === '/' || currentPath === '/app' || currentPath === '#'){
-      alert('aaaaa');
       pushed = true;
       this.$router.push('/app/landing');
     }
 
     if(currentPath === '/dao' || JSON.stringify(currentPath).indexOf('/dao') !== -1) {
-      alert('dao');
       pushed = true;
       window.localStorage.setItem('authenticated', true);
       this.$router.push('/dao/dashboard');
     }
 
     if(!pushed){
-      alert('current path: '+currentPath);
       this.$router.push(currentPath);
     }
   },
